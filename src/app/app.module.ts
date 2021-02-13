0f55c166db4241cd8c277a09b829f7cf1a7c10b3
garlife <garlife@gmail.com>
2021-02-13 12:42:44 +0300

    ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { Mod1Module } from './mod1/mod1.module';
 import { Mod2Module } from './mod2/mod2.module';
 import { Comp1Component } from './comp1/comp1.component';
 import { CommonModule } from '@angular/common';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
